4b7efec8b645ac07281210f989c01d49b5d294e8
Sean Choi <sean@rankone.io>
2023-03-21 23:59:59 -0700

    Initial commit

---
+++ personal-site/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="center">
+    <img src="@/assets/Obamahedron.jpeg">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomeView',
+  
+});
+</script>
+
+<style scoped>
+.center {
+  display: flex;
+  height: 100%;
+  width: 100%;
+
+  align-items: center;
+}
+</style>
