604e45cbfe511ef54563b96d33284709b533b963
Sean Choi <sean@rankone.io>
2023-03-30 23:29:59 -0600

    Added a Korean name typing effect

--- personal-site/src/components/CoolNameComponent.vue
+++ personal-site/src/components/CoolNameComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 @mouseover="typeEffect" @mouseleave="eraseEffect">
+  <h1 @mouseover="alternateTypeEffect">
     {{ displayName }}{{ blinkingBar }}
   </h1>
 </template>
@@ -34,31 +34,56 @@ function randomInteger(min: number, max: number): number {
 const isTyping = ref<boolean>(false);
 const isFinished = ref<boolean>(false);
 
-async function typeEffect() {
+const alternate = ref<boolean>(false);
+async function alternateTypeEffect() {
   if (isTyping.value || isFinished.value ) return;
 
+  await eraseEffect();
+
   isTyping.value = true;
-  for (let c of literalName) {
-    displayName.value = displayName.value.concat(c);
-    await delay(randomInteger(50, 150));
+
+  if (alternate.value) {
+    await typeEffectEnglish();
+  } else {
+    await typeEffectKorean();
   }
+  alternate.value = !alternate.value;
 
   isTyping.value = false;
-  isFinished.value = true;
+}
+
+
+async function typeEffectEnglish() {
+  for (let c of literalName) {
+    displayName.value = displayName.value.concat(c);
+    await delay(randomInteger(30, 50));
+  }
+}
+
+async function typeEffectKorean() {
+  var sequence1 = ['ㅊ', '초', '최'];
+  var sequence2 = ['ㅅ', '스', '승'];
+  var sequence3 = ['ㅇ', '우'];
+  var sequences = [sequence1, sequence2, sequence3];
+
+  for (let i = 0; i < sequences.length; i++) {
+    let current = sequences[i];
+    for (let c of current) {
+      displayName.value = displayName.value.substring(0, i).concat(c);
+      await delay(randomInteger(30, 50));
+    }
+  }
 }
 
 async function eraseEffect() {
-  if (isTyping.value || !isFinished.value ) return;
-
   isTyping.value = true;
   while(displayName.value) {
     displayName.value = displayName.value.substring(0, displayName.value.length - 1);
     await delay(30);
   }
-
-  isTyping.value = false;
-  isFinished.value = false;
 }
+  
+
 </script>
 
 <style>
